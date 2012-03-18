e64bd969835ecf6956cb202888bd428536613f1f
wenjiaruan <wenjiaruan@gmail.com>
2012-03-18 20:09:33 +0000

    Add a CohortsLF, which is CohortsLazy with Filter

--- libstm/algs/algs.hpp
+++ libstm/algs/algs.hpp
@@ -46,7 +46,7 @@ namespace stm
       OrecLazy,  OrecLazyHour,  OrecLazyBackoff,  OrecLazyHB,
       NOrec,     NOrecHour,     NOrecBackoff,     NOrecHB,
       Wealth, Cohorts, Cohortsnoorder, Cohortsold, CohortsLazy, CohortsEager,
-      CohortsLI, CohortsNOrec, CohortsLN, CohortsFilter,
+      CohortsLI, CohortsNOrec, CohortsLN, CohortsFilter, CohortsLF,
       // ProfileTM support.  These are not true STMs
       ProfileTM, ProfileAppAvg, ProfileAppMax, ProfileAppAll,
       // end with a distinct value
