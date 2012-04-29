321d68eee598f4609cfbbf09eaf8bc3035052947
wenjiaruan@gmail.com <wenjiaruan@gmail.com@d8c8c26f-3c19-d1f2-4759-8dc24d6bf138>
2012-04-29 04:06:33 +0000

    add CTokenNOrec

--- branches/wenjia/libstm/algs.hpp
+++ branches/wenjia/libstm/algs.hpp
@@ -47,7 +47,7 @@ namespace stm
       Wealth, Cohorts, Cohortsnoorder, Cohortsold, CohortsLazy, CohortsEager,
       CohortsLI, CohortsNOrec, CohortsLN, CohortsFilter, CohortsLF, CohortsEF,
       Fastlane1, Fastlane2, PTM, FastlaneSwitch, CTokenTurboELA, CohortsEN,
-      CohortsEN2,
+      CohortsEN2,CTokenNOrec,
       // ProfileTM support.  These are not true STMs
       ProfileTM, ProfileAppAvg, ProfileAppMax, ProfileAppAll,
       // end with a distinct value
