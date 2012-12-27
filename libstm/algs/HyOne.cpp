80c3f54d539f63e2ff0df6d96f1a0a6702a9fece
albertwang87 <albertwang87@d8c8c26f-3c19-d1f2-4759-8dc24d6bf138>
2012-12-27 17:09:01 +0000

    three tsx functions added

--- libstm/algs/HyOne.cpp
+++ libstm/algs/HyOne.cpp
@@ -25,6 +25,48 @@
  *  Declare the functions that we're going to implement, so that we can avoid
  *  circular dependencies.
  */
+
+unsigned int _xbegin(void); 
+void _xend(void); 
+void _xabort(void); 
+
+#define _XBEGIN_STARTED (~0u) 
+#define _XABORT_EXPLICIT (1 << 0) 
+#define _XABORT_RETRY (1 << 1) 
+#define _XABORT_CONFLICT (1 << 2) 
+#define _XABORT_CAPACITY (1 << 3) 
+#define _XABORT_DEBUG (1 << 4) 
+#define _XABORT_NESTED (1 << 5)
+
+//starts a RTM code region and returns a value indicating transaction successfully started or status from a transaction abort.
+
+//If the logical processor was not already in transactional execution, then the xbegin instruction causes the logical processor to start transactional execution. The xbegin instruction that transitions the logical processor into transactional execution is referred to as the outermost xbegin instruction.
+
+//The xbegin instruction specifies a relative offset to the fallback code path executed following a transactional abort. To promote proper program structure, this is not exposed in C++ code and the intrinsic function operates as if it invoked the following model code
+  inline unsigned int _xbegin(void)
+  {
+	unsigned status;
+	asm volatile ("movl $0xffffffff, %%eax;"
+		      ".byte 0xc7 \n\t .byte 0xf8 \n\t __txnL1"
+		      "__txnL1: movl %%eax, %0;"
+			:"=r"(status));
+	return status;
+  }
+
+//Specifies the end of restricted transactional memory code region. If this is the outermost transaction (including this xend instruction, the number of xbegin matches the number of xend instructions) then the processor will attempt to commit processor state automatically.
+  inline void _xend(void)
+  {
+	asm volatile (".byte 0x0f \n\t .byte 0x01 \n\t .byte 0xd5;");
+  }
+
+//Forces a RTM region to abort. All outstanding transactions are aborted and the logical processor resumes execution at the fallback address computed through the outermost xbegin transaction.
+
+//The EAX register is updated to reflect an xabort instruction caused the abort, and the imm8 argument will be provided in the upper eight bits of the return value (EAX register bits 31:24) containing the indicated immediate value. The argument of xabort function must be a compile time constant.
+  inline void _xabort(void)
+  {
+	asm volatile (".byte 0xc6 \n\t .byte 0xf8 \n\t .byte 0x12");
+  }
+
 namespace stm
 {
   /**
@@ -54,12 +96,11 @@ namespace stm
 	timestamp.val = 0;
 	tx->hyOne_abort_count = 0;
       }
-
       // otherwise
       else
       {
         //execute XEND
-	asm volatile (".byte 0x0f \n\t .byte 0x01 \n\t .byte 0xd5");
+	_xend();
 
       }
       // finalize mm ops, and log the commit
@@ -112,88 +153,94 @@ namespace stm
    *    We need a zero timestamp, so we need to save its max value to support
    *    algorithms that do not expect the timestamp to ever decrease
    */
-  void HyOneOnSwitchTo()
-  {
-      timestamp_max.val = MAXIMUM(timestamp.val, timestamp_max.val);
-      timestamp.val = 0;
-  }
+	void HyOneOnSwitchTo()
+	{
+	timestamp_max.val = MAXIMUM(timestamp.val, timestamp_max.val);
+	timestamp.val = 0;
+	}
 
-  void HyOneBegin(TX_LONE_PARAMETER);
+	void HyOneBegin(TX_LONE_PARAMETER);
 
-	// Note that XBEGIN requires an abort handler.  Ours should set the
-	// flag to true and then re-call HyOneBegin 
 	void HyOneAbort(TX_LONE_PARAMETER)
 	{
-	  TX_GET_TX_INTERNAL;
-	  tx->hyOne_abort_count ++;
-	  while (timestamp.val == 1) {}
+	
+		// Note that XBEGIN requires an abort handler.  Ours should set the
+		// flag to true and then re-call HyOneBegin 	
+		TX_GET_TX_INTERNAL;
+		tx->hyOne_abort_count ++;
+		while (timestamp.val == 1) {}
 
-	  return HyOneBegin(TX_LONE_PARAMETER);
+		HyOneBegin(TX_LONE_PARAMETER);
 	}
-  /**
-   *  HyOne begin:
-   *
-   *    We grab the lock, but we count how long we had to spin, so that we can
-   *    possibly adapt after releasing the lock.
-   *
-   *    This is external and declared in algs.hpp so that we can access it as a
-   *    default in places.
-   */
-  void HyOneBegin(TX_LONE_PARAMETER)
-  {
-      TX_GET_TX_INTERNAL;
+	/**
+	*  HyOne begin:
+	*
+	*    We grab the lock, but we count how long we had to spin, so that we can
+	*    possibly adapt after releasing the lock.
+	*
+	*    This is external and declared in algs.hpp so that we can access it as a
+	*    default in places.
+	*/
+	void HyOneBegin(TX_LONE_PARAMETER)
+	{
+		TX_GET_TX_INTERNAL;
 
-      // [cw] we will need to add a flag to the TxThread object in the
-      // txthread.hpp file.  The flag will be a bool that is true if we are
-      // supposed to start in software mode, and false otherwise
+		// [cw] we will need to add a flag to the TxThread object in the
+		// txthread.hpp file.  The flag will be a bool that is true if we are
+		// supposed to start in software mode, and false otherwise
 
-      // [cw]
-      // algorithm here is:
-      //
-      // if (flag == true) then acquire the lcok
-      // get the lock and notify the allocator	
-      tx->allocator.onTxBegin();
-	//asm volatile();
-	      // else
-      		//   issue an XBEGIN instruction
-      		//   then spin until the lock is unheld
+		// [cw]
+		// algorithm here is:
+		//
+		// if (flag == true) then acquire the lcok
+		// get the lock and notify the allocator	
+		tx->allocator.onTxBegin();
+		//asm volatile();
+		      // else
+			//   issue an XBEGIN instruction
+			//   then spin until the lock is unheld
 
-	if (tx->nesting_depth++)
-	//we are already in a transaction context
-	//therefore, we do nothing, just return to a outside transaction
-		return;
-	//xbegin __abort_handler
-	asm volatile (".byte 0xc7 \n\t .byte 0xf8 \n\t __abort_handler"
-		      "__abort_handler: nop;"
-				       "call HyOneAbort;");
-      
-      // we use the global timestamp as a lock for the Serial PhaseTM
-      // If this lock is occupied by another transaction, it means that 
-      // another transaction is using the resource exclusively in the 
-      // language sseiral mode, so we have to abort.
-	      if (timestamp.val == 1)
-	      {
-		  //XABORT
-		  asm volatile (".byte 0xc6 \n\t .byte 0xf8 \n\t .byte 0x12");
-	      }
+		if (tx->nesting_depth++)
+		//we are already in a transaction context
+		//therefore, we do nothing, just return to a outside transaction
+			return;
 
-      // if we have aborted for more than 8 times, then we grab the lock,
-      // ending the hardware transaction mode and entering the language 
-      // serial mode. After this, all other hardware transactions are forced to
-      // abort. Note that either all hardware transactions run concurrently, 
-      // or one hardware transaction is running with the lock owned and all
-      // others have to wait until the lock is freed before starting another
-      // hardware transaction
+		unsigned int status;	
+		if ((status = _xbegin()) == _XBEGIN_STARTED)
+		{
+			// we use the global timestamp as a lock for the Serial PhaseTM
+			// If this lock is occupied by another transaction, it means that 
+			// another transaction is using the resource exclusively in the 
+			// language sseiral mode, so we have to abort.
+			if (timestamp.val == 1)
+			{
+				//XABORT
+				_xabort();
 
-	      if (tx->hyOne_abort_count > 8)
-	      {
-		timestamp.val = 1;
-		//XEND
-		asm volatile (".byte 0x0f \n\t .byte 0x01 \n\t .byte 0xd5");
-		tx->irrevoc = 1;
-	      }
+			}
 
-   }	
+			// if we have aborted for more than 8 times, then we grab the lock,
+			// ending the hardware transaction mode and entering the language 
+			// serial mode. After this, all other hardware transactions are forced to
+			// abort. Note that either all hardware transactions run concurrently, 
+			// or one hardware transaction is running with the lock owned and all
+			// others have to wait until the lock is freed before starting another
+			// hardware transaction
+
+			if (tx->hyOne_abort_count > 8)
+			{
+				timestamp.val = 1;
+				//XEND
+				_xend();
+				tx->irrevoc = 1;
+			}
+		}
+		else  //transaction fails to start, or abort. this is the fallback execution path
+		{
+			HyOneAbort(TX_LONE_PARAMETER);		
+		}
+
+	}	
 
 }
 
