5d891d32ab50ed84ef88976f14a1384f8a3367ac
albertwang87 <albertwang87@d8c8c26f-3c19-d1f2-4759-8dc24d6bf138>
2013-01-02 06:33:46 +0000

    cleaner comments

--- libstm/algs/HyOne.cpp
+++ libstm/algs/HyOne.cpp
@@ -13,9 +13,6 @@
  *
  *    This is the classic STM baseline: there is no instrumentation, as all
  *    transactions are protected by the same single test-and-test-and-set lock.
- *
- *    NB: retry and restart are not supported, and we never know if a
- *        transaction is read-only or not
  */
 
 #include "algs.hpp"
@@ -57,23 +54,24 @@ void _xabort(void);
 
 // [mfs] We should rename this to TSX_BEGIN, then employ an ifdef that
 //       defines HTM_BEGIN as TSX_BEGIN
-#define TSX_BEGIN      \
-	".byte 0xc7 \n\t" \
-	".byte 0xf8 \n\t" \
-	".byte 0x00 \n\t" \
-	".byte 0x00 \n\t" \
-	".byte 0x00 \n\t" \
-	".byte 0x00 \n\t"
+#define TSX_BEGIN				\
+    ".byte 0xc7 \n\t"                           \
+    ".byte 0xf8 \n\t"                           \
+    ".byte 0x00 \n\t"				\
+    ".byte 0x00 \n\t"				\
+    ".byte 0x00 \n\t"				\
+    ".byte 0x00 \n\t"
 
-#define TSX_END	  \
-	".byte 0x0f \n\t" \
-	".byte 0x01 \n\t" \
-	".byte 0xd5"
+#define TSX_END					\
+    ".byte 0x0f \n\t"				\
+    ".byte 0x01 \n\t"				\
+    ".byte 0xd5 \n\t"
 
-#define TSX_ABORT	  \
-	".byte 0xc6 \n\t" \
-	".byte 0xf8 \n\t" \
-	".byte 0x12"
+#define TSX_ABORT				\
+    ".byte 0xc6 \n\t"				\
+    ".byte 0xf8 \n\t"				\
+    ".byte 0x12 \n\t"
+
 /**
  *  [mfs] Should have a description here of whatever is going on.  In this
  *        case, it looks like what we are doing is having a status variable
@@ -86,21 +84,12 @@ void _xabort(void);
 inline unsigned int _xbegin(void)
 {
     unsigned status;
->>>>>>> .r593
-
-<<<<<<< .mine
-
-  inline unsigned int _xbegin(void)
-  {
-	unsigned status;
-=======
     // [mfs] I presume this is debug code that needs to go away?
     fprintf(stdout, "into xbegin\n");
->>>>>>> .r593
 
     // [mfs] Should we be using _XBEGIN_STARTED here?
     asm volatile("movl $0xffffffff, %%eax \n\t"
-                 INTEL_XBEGIN " \n\t"
+                 TSX_BEGIN " \n\t"
                  "movl %%eax, %0"
                  :"=r"(status));
 
@@ -110,13 +99,6 @@ inline unsigned int _xbegin(void)
     return status;
 }
 
-<<<<<<< .mine
-//Specifies the end of restricted transactional memory code region. If this is the outermost transaction (including this xend instruction, the number of xbegin matches the number of xend instructions) then the processor will attempt to commit processor state automatically.
-  inline void _xend(void)
-  {
-	asm volatile (INTEL_XEND " \n\t");
-  }
-=======
 // Specifies the end of restricted transactional memory code region. If this
 // is the outermost transaction (including this xend instruction, the number
 // of xbegin matches the number of xend instructions) then the processor will
@@ -125,13 +107,10 @@ inline unsigned int _xbegin(void)
 // [mfs] I think we make sure in our code to never use the hardware nesting
 //       stuff... is that correct?
 //
-// [mfs] This isn't very orthogonal... why aren't the ASM instructions in a
-//       define, as in INTEL_XBEGIN?
 inline void _xend(void)
 {
-    asm volatile(".byte 0x0f \n\t .byte 0x01 \n\t .byte 0xd5;");
+    asm volatile(TSX_END " \n\t");
 }
->>>>>>> .r593
 
 // Forces an RTM region to abort. All outstanding transactions are aborted
 // and the logical processor resumes execution at the fallback address
@@ -147,19 +126,9 @@ inline void _xend(void)
 //       as?  Can we elevate the value to a constant of some sort?
 inline void _xabort(void)
 {
-    // [mfs] this ASM should be in a define...
-    asm volatile(".byte 0xc6 \n\t .byte 0xf8 \n\t .byte 0x12");
+    asm volatile(TSX_ABORT " \n\t");
 }
 
-<<<<<<< .mine
-//The EAX register is updated to reflect an xabort instruction caused the abort, and the imm8 argument will be provided in the upper eight bits of the return value (EAX register bits 31:24) containing the indicated immediate value. The argument of xabort function must be a compile time constant.
-  inline void _xabort(void)
-  {
-	asm volatile (INTEL_XABORT " \n\t");
-  }
-
-=======
->>>>>>> .r593
 namespace stm
 {
   /**
@@ -170,14 +139,8 @@ namespace stm
   {
       // [mfs] debug message, should be removed...
       fprintf(stdout, "Commit!!\n");
+      
       TX_GET_TX_INTERNAL;
-
-      // [mfs] We probably don't need these comments anymore...
-      //
-      // the variable nesting_depth indicates the depth of current
-      // transaction.  if the current transaction is running inside another
-      // outside transaction, just return so that we don't have to do the
-      // actual commit.
 
       // [mfs] Why are we asserting this?
       assert(tx->nesting_depth == 0);
@@ -185,12 +148,7 @@ namespace stm
       // [mfs] Are these comments still needed?  If so, they should probably
       //       be moved up to the method's comment, so that the algorithm is
       //       cleanly documented in one place, rather than inline.
-      //
-      // if (--tx->nesting_depth) return;
-      // [cw]
-      // new algorithm:
-      // if (flag is true)
-      //   release the lock and set flag to false
+      
       // the variable irrevoc represents the modes this PhaseTM-serial transaction is currently working.
       // irrevoc == 1 means that it now runs in language serial mode.
       // irrevoc == 0 means that it now runs in hardware mode.
@@ -200,8 +158,6 @@ namespace stm
       // the hardware TM right now, and it holds a lock.  We can just release
       // the lock to commit."
       if (tx->irrevoc) {
-          // [mfs] Do we need this comment?
-          // tatas_release(&timestamp.val);
           tx->irrevoc = false;
           timestamp.val = 0;
           tx->hyOne_abort_count = 0;
@@ -213,8 +169,10 @@ namespace stm
       // [mfs] I am worried about us not handling mm correctly.  Why aren't
       // we calling OnCGLCommit(tx) here?
 
+      // [chw] I am still unclear why we have to call this, but not some other functions like OnRWCommit(tx);
+      // BTW, isn't CGL another algorithm? Why do we have to call a function related to **another** algorithm?
       // finalize mm ops, and log the commit
-      //      OnCGLCommit(tx);
+      OnCGLCommit(tx);
   }
 
   /**
@@ -235,13 +193,6 @@ namespace stm
       STM_DO_MASKED_WRITE(addr, val, mask);
   }
 
-  /**
-   *  HyOne unwinder:
-   *
-   *    In HyOne, aborts are never valid
-   *
-   *    [mfs] Why?
-   */
   void HyOneRollback(STM_ROLLBACK_SIG(,,))
   {
       UNRECOVERABLE("ATTEMPTING TO ABORT AN IRREVOCABLE HyOne TRANSACTION");
@@ -274,50 +225,14 @@ namespace stm
       timestamp.val = 0;
   }
 
-  // [mfs] Why do we need the forward declaration here?
-  void HyOneBegin(TX_LONE_PARAMETER);
-
-<<<<<<< .mine
-	void HyOneAbort(TX_LONE_PARAMETER)
-	{
-	
-		// Note that XBEGIN requires an abort handler.  Ours should set the
-		// flag to true and then re-call HyOneBegin 	
-		TX_GET_TX_INTERNAL;
-		tx->hyOne_abort_count ++;
-		while (timestamp.val == 1) {}
-=======
   void HyOneAbort(TX_LONE_PARAMETER)
   {
       // Note that XBEGIN requires an abort handler.  Ours should set the
       // flag to true and then re-call HyOneBegin
       TX_GET_TX_INTERNAL;
       tx->hyOne_abort_count++;
->>>>>>> .r593
 
-<<<<<<< .mine
-		HyOneBegin(TX_LONE_PARAMETER);
-	}
-	/**
-	*  HyOne begin:
-	*
-	*    We grab the lock, but we count how long we had to spin, so that we can
-	*    possibly adapt after releasing the lock.
-	*
-	*    This is external and declared in algs.hpp so that we can access it as a
-	*    default in places.
-	*/
-	void HyOneBegin(TX_LONE_PARAMETER)
-	{
-		TX_GET_TX_INTERNAL;
-		
-		fprintf(stdout, "begin\n");
-=======
-      // [mfs] I thought we needed to spin here... why is this code commented
-      // out?
-      //
-      // while (timestamp.val == 1) {}
->>>>>>> .r593
+      while (timestamp.val == 1) {}
 
       // [mfs] I don't quite follow the control flow here.  There is no call
       //       to tmabort(), so how is the stack getting rolled back?  How
@@ -327,7 +242,7 @@ namespace stm
       //       things, and since we are in an abort situation, a setjmp is
       //       effectively "backoff"...
 
-      //      HyOneBegin(TX_LONE_PARAMETER);
+      tmabort();
   }
 
   /**
@@ -341,76 +256,23 @@ namespace stm
    */
   void HyOneBegin(TX_LONE_PARAMETER)
   {
-      TX_GET_TX_INTERNAL;
+	TX_GET_TX_INTERNAL;
 
-<<<<<<< .mine
-		fprintf(stdout, "begin2\n");
-		unsigned int status;	
-		if ((status = _xbegin()) == _XBEGIN_STARTED)
-		{
-			// we use the global timestamp as a lock for the Serial PhaseTM
-			// If this lock is occupied by another transaction, it means that 
-			// another transaction is using the resource exclusively in the 
-			// language sseiral mode, so we have to abort.
-			
-			fprintf(stdout, "begin3 status=%u\n", status);
-			if (timestamp.val == 1)
-			{
-				//XABORT
-				_xabort();
-=======
-      // [mfs] erroneous debug message?
-      fprintf(stdout, "begin\n");
->>>>>>> .r593
+	fprintf(stdout, "begin2\n");
 
-      // [mfs] Comments about the algorithm behavior should be in the
-      // function header.
+      	// [mfs] Do we need this assert?
+      	assert (tx->nesting_depth == 1);
+      
+	//we are already in a transaction context
+	//therefore, we do nothing, just return to a outside transaction
 
-      // [cw] we will need to add a flag to the TxThread object in the
-      // txthread.hpp file.  The flag will be a bool that is true if we are
-      // supposed to start in software mode, and false otherwise
-      //
-      // [mfs] Is the flag added?  What is it called?  Is it even needed
-      //       anymore?
+	unsigned int status = _xbegin();
+	if (status == _XBEGIN_STARTED) {
 
-      // [mfs] I am worried about us not calling this function:
-      //      tx->allocator.onTxBegin();
-
-      // [cw]
-      // algorithm here is:
-      //
-      // if (flag == true) then acquire the lcok
-      // get the lock and notify the allocator
-      //asm volatile();
-      // else
-      //   issue an XBEGIN instruction
-      //   then spin until the lock is unheld
-
-<<<<<<< .mine
-			fprintf(stdout, "begin5, status=%u\n", status);
-			HyOneAbort(TX_LONE_PARAMETER);		
-		}
-	}	
-=======
-      // [mfs] Do we need this assert?
-      assert (tx->nesting_depth == 1);
-      //we are already in a transaction context
-      //therefore, we do nothing, just return to a outside transaction
->>>>>>> .r593
-
-      // [mfs] erroneous debug message?
-      fprintf(stdout, "begin2\n");
-
-      // [mfs] It's probably cleaner to say 'unsigned int status = _xbegin();'
-      unsigned int status;
-      if ((status = _xbegin()) == _XBEGIN_STARTED) {
           // we use the global timestamp as a lock for the Serial PhaseTM
           // If this lock is occupied by another transaction, it means that
           // another transaction is using the resource exclusively in the
-          // language sseiral mode, so we have to abort.
-
-          // [mfs] Unnecessary debug message
-          fprintf(stdout, "begin3\n");
+          // language sseiral mode, so we have to abort
 
 
           // [mfs] Comment here should indicate what is going to happen:
